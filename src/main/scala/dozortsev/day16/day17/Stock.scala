242490590cff430ed54e06d9ceeefbbde37d2bdc
Anton <dozortsevanton@gmail.com>
2015-08-20 21:39:31 +0300

    Preventing generation get and set methods for class field

---
+++ src/main/scala/dozortsev/day16/day17/Stock.scala
@@ -0,0 +1,23 @@
+package dozortsev.day16.day17
+
+class Stock {
+
+  // this field visible for any Stock instances
+  // as this member is modified as private getter and setter methods won't be generated
+  private var price: Double = _
+
+  def setPrice(price: Double) = this.price = price
+
+  def isHigher(that: Stock) = this.price > that.price
+}
+
+object Driver extends App {
+
+  val foo = new Stock
+  val bar = new Stock
+
+  foo.setPrice(12.6)
+  bar.setPrice(17.12)
+
+  assert(bar isHigher foo)
+}
