3ac80640d65fc48634d2cce7a68a2630f9454de1
Anton <dozortsevanton@gmail.com>
2015-08-29 20:08:01 +0300

    Put import statement anywhere

--- src/main/scala/dozortsev/day19/Main.scala
+++ src/main/scala/dozortsev/day19/Main.scala
@@ -1,10 +1,11 @@
 package dozortsev.day19
 
 import java.util.{ArrayList => JList}   // rename class in import process
-import scala.Predef.{println => p}      // provide shortcut alias on a exist method
 
 object Main {
   def main(args: Array[String]) {
+
+    import scala.Predef.{println => p}  // put import statement anywhere
 
     val ints = new JList[Int]()
     ints add 12
