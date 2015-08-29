578e9348f088687b9c0e3469941239272e617e50
Anton <dozortsevanton@gmail.com>
2015-08-29 17:29:39 +0300

    Renaming classes and their members in import statement

---
+++ src/main/scala/dozortsev/day19/Main.scala
@@ -0,0 +1,15 @@
+package dozortsev.day19
+
+import java.util.{ArrayList => JList}   // rename class in import process
+import scala.Predef.{println => p}      // provide shorthand alias on a exist method
+
+object Main {
+  def main(args: Array[String]) {
+
+    val ints = new JList[Int]()
+    ints add 12
+    ints add 100
+
+    p(ints)
+  }
+}
